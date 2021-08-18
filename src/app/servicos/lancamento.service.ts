c24d3b2d998757a883570a6a5bd0a757ad2b0d88
Ythalo-G <galvaniythalo@gmail.com>
2021-08-18 16:29:47 -0300

    Adicionado download de PDF

--- src/app/servicos/lancamento.service.ts
+++ src/app/servicos/lancamento.service.ts
@@ -5,6 +5,10 @@ import { environment as env } from 'src/environments/environment';
 import { Lancamento } from '../models/lancamentos.model';
 import { HttpService } from './http.service';
 import { saveAs } from 'file-saver';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -35,6 +39,42 @@ export class LancamentoService {
     const dados = colunas + linhas.join('\n');
     const blob = new Blob([dados], { type: 'text/csv' });
     saveAs(blob, 'lancamentos.csv');
+  }
+
+  downloadPDF(lancamentos: Lancamento[]){
+     const colunas = ['ID', 'DATA', 'HORA', 'TIPO', 'LOCALIZACAO'];
+     const linhas: string[][] = [];
+     lancamentos.forEach(lanc => {
+      const dataHora = lanc.data.split(' ');
+      linhas.push([
+        lanc.id.toString(),
+        dataHora[0],
+        dataHora[1],
+        lanc.tipo,
+        lanc.localizacao
+      ]);
+    });
+    const data = new Date();
+   // const doc = new jsPDF('l', 'mm', 'a1');--ALMENTAR A TABELAA
+    const doc = new jsPDF();
+    (doc as any).text(15,10, 'Listagem de lançamentos');
+    (doc as any).setFontSize(10);
+    (doc as any).text(170,10, data.toLocaleString('pt-BR'));
+   
+   
+    (doc as any).autoTable({
+      startY: 20,
+      head: [colunas],
+      body: linhas,
+      foot:[colunas],
+      theme:'grid' //mudar tema da tabela
+    });
+    
+    
+    doc.save('lancamentos' + data.toLocaleDateString('pt-BR') + '.pdf');
+    //const nome = window.prompt();
+    //doc.save(nome?.toString()); pede nome 
+   
   }
 
 }
